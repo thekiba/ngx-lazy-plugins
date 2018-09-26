9a17cffa4060bdb6580ba5b85baab138d2045317
Jamaks <jamakrus@gmail.com>
2018-09-26 23:15:57 +0300

    remove ngx-core

--- projects/ngx-lazy/core/src/lib/state/model.ts
+++
@@ -1,4 +0,0 @@
-export class Toolbar {
-    title: string;
-    route: string;
-}
--- projects/ngx-lazy/core/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of core
- */
-export * from './lib/state/index';
-export * from './lib/state/action';
-export * from './lib/core.module';
