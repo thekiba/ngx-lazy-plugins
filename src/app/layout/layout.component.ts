aae5ff18f7cd26ac80cf63de02b84b8a6c71bf9d
Jamaks <jamakrus@gmail.com>
2018-09-27 16:30:31 +0300

    add loader plugins

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { ToolbarService, CartService } from 'src/core';
 import { LoaderService } from '../loader.service';
 import { PluginsService, PluginInfo } from '../service/plugins.service';
+import { ClrLoadingState } from '@clr/angular';
 
 @Component({
     selector: 'app-layout',
@@ -13,7 +14,8 @@ export class LayoutComponent implements OnInit, OnDestroy {
     showmenu = false;
     links = [];
     cartItems = [];
-    plugins: PluginInfo[];
+    plugins: any;
+    specificstate = ClrLoadingState.DEFAULT;
     sub = new Subscription();
     name: string;
     url: string;
@@ -32,11 +34,28 @@ export class LayoutComponent implements OnInit, OnDestroy {
         this.sub.add(subCart);
         this.pluginsService.getPlugins().subscribe(data => {
             this.plugins = data;
+            this.plugins.map(el => el['state'] = ClrLoadingState.DEFAULT);
         });
         this.sub.add(subToolbar);
     }
     ngOnDestroy() {
         this.sub.unsubscribe();
+    }
+    loadEssential(plugin) {
+        plugin.state = ClrLoadingState.LOADING;
+        this.load(plugin.url, plugin.name);
+        // only demo :)
+        setTimeout(() => {
+            plugin.state = ClrLoadingState.SUCCESS;
+        }, 1500);
+
+    }
+    loadSpecific(url: string, name: string) {
+        this.specificstate = ClrLoadingState.LOADING;
+        this.loader.load(url, name);
+        setTimeout(() => {
+            this.specificstate = ClrLoadingState.SUCCESS;
+        }, 1500);
     }
     load(url: string, name: string) {
         this.loader.load(url, name);
